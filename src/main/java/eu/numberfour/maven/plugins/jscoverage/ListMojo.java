7e996e9b6fa1f0879ba24c9366001f912b9384d5
Valentino Pinna <valentino.pinna@bancaditalia.it>
2017-12-06 11:59:19 +0100

    Added "fields to print" parameter

--- src/main/java/eu/numberfour/maven/plugins/jscoverage/ListMojo.java
+++ src/main/java/eu/numberfour/maven/plugins/jscoverage/ListMojo.java
@@ -2,9 +2,16 @@ package eu.numberfour.maven.plugins.jscoverage;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileTime;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Date;
+import java.util.TimeZone;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -13,12 +20,14 @@ import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.util.DirectoryScanner;
 
-import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 /**
  * <p>
- * Creates a JSON formatted list of files form a base directory, and optional includes and excludes.
+ * Creates a JSON formatted list of files form a base directory, and optional
+ * includes and excludes.
  * </p>
  * 
  * @author <a href="mailto:leonard.ehrenfried@web.de">Leonard Ehrenfried</a>
@@ -29,137 +38,160 @@ import com.google.gson.GsonBuilder;
  */
 public class ListMojo extends AbstractMojo {
 
-    public static final String NEW_LINE = System.getProperty("line.separator");
-    public static final String FILE_SEPARATOR = System.getProperty("file.separator");
+	public static final String NEW_LINE = System.getProperty("line.separator");
+	public static final String FILE_SEPARATOR = System.getProperty("file.separator");
 
-    /**
-     * The Maven project.
-     * 
-     * @parameter expression="${project}"
-     * @required
-     * @readonly
-     */
-    private MavenProject project;
-    /**
-     * File into which to save the output of the transformation.
-     * 
-     * @parameter default-value="${basedir}/target/file-list.json"
-     */
-    private String outputFile;
-    /**
-     * Base directory of the scanning process
-     * 
-     * @parameter default-value="${basedir}/target/"
-     */
-    public String baseDir;
-    /**
-     * Ant-style include pattern.
-     * 
-     * For example **.* is all files
-     * 
-     * @parameter
-     */
-    public String[] includes;
-    /**
-     * Ant-style exclude pattern.
-     * 
-     * For example **.* is all files
-     * 
-     * @parameter
-     */
-    public String[] excludes;
+	/**
+	 * The Maven project.
+	 * 
+	 * @parameter expression="${project}"
+	 * @required
+	 * @readonly
+	 */
+	private MavenProject project;
+	/**
+	 * File into which to save the output of the transformation.
+	 * 
+	 * @parameter default-value="${basedir}/target/file-list.json"
+	 */
+	private String outputFile;
+	/**
+	 * Base directory of the scanning process
+	 * 
+	 * @parameter default-value="${basedir}/target/"
+	 */
+	public String baseDir;
+	/**
+	 * Ant-style include pattern.
+	 * 
+	 * For example **.* is all files
+	 * 
+	 * @parameter
+	 */
+	public String[] includes;
+	/**
+	 * Ant-style exclude pattern.
+	 * 
+	 * For example **.* is all files
+	 * 
+	 * @parameter
+	 */
+	public String[] excludes;
 
-    /**
-     * @parameter default-value="json"
-     */
-    public String type;
+	/**
+	 * Fields to print.
+	 * 
+	 * Allowed fields: name, size, creationTime, lastModifiedFile
+	 * 
+	 * @parameter
+	 */
+	public String[] fields = { "name" };
 
-    /**
-     * Whether to ignore case
-     * 
-     * @parameter
-     */
-    public boolean caseSensitive;
+	/**
+	 * @parameter default-value="json"
+	 */
+	public String type;
 
-    @Override
-    public void execute() throws MojoExecutionException, MojoFailureException {
-        FileWriter fileWriter = null;
-        try {
+	/**
+	 * Whether to ignore case
+	 * 
+	 * @parameter
+	 */
+	public boolean caseSensitive;
 
-            Log log = getLog();
-            log.info("");
-            log.info("Creating file list ");
-            log.info("Basedir:  " + this.baseDir);
-            log.info("Output:   " + this.outputFile);
-            log.info("Includes: " + Arrays.toString(this.includes));
-            log.info("Exludes:  " + Arrays.toString(this.excludes));
-            log.info("Type:  " + this.type);
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
 
-            DirectoryScanner scanner = new DirectoryScanner();
-            scanner.setBasedir(this.baseDir);
-            scanner.setIncludes(this.includes);
-            scanner.setExcludes(this.excludes);
-            scanner.setCaseSensitive(this.caseSensitive);
-            scanner.scan();
+		Log log = getLog();
+		log.info("");
+		log.info("Creating file list ");
+		log.info("Basedir:  " + this.baseDir);
+		log.info("Output:   " + this.outputFile);
+		log.info("Includes: " + Arrays.toString(this.includes));
+		log.info("Exludes:  " + Arrays.toString(this.excludes));
+		log.info("Type:  " + this.type);
 
-            String[] includedFiles = scanner.getIncludedFiles();
+		DirectoryScanner scanner = new DirectoryScanner();
+		scanner.setBasedir(this.baseDir);
+		scanner.setIncludes(this.includes);
+		scanner.setExcludes(this.excludes);
+		scanner.setCaseSensitive(this.caseSensitive);
+		scanner.scan();
 
-            log.info("File list contains " + includedFiles.length + " files");
+		JsonArray array = new JsonArray();
+		FileSystem fileSystem = FileSystems.getDefault();
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
+		dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
 
-            fileWriter = new FileWriter(this.outputFile);
+		try (FileWriter fileWriter = new FileWriter(this.outputFile)) {
 
-            if ("json".equals(this.type)) {
-                Gson gson = new GsonBuilder().setPrettyPrinting().create();
-                String json = gson.toJson(includedFiles);
-                fileWriter.write(json);
-            } else if ("junit".equals(this.type)) {
-                StringBuilder suiteContent = new StringBuilder();
-                suiteContent.append("package n4.quat.selenium.acceptancetest.suites;");
-                suiteContent.append(NEW_LINE);
-                suiteContent.append(NEW_LINE);
-                suiteContent.append("import org.junit.runner.RunWith;");
-                suiteContent.append(NEW_LINE);
-                suiteContent.append("import org.junit.runners.Suite.SuiteClasses;");
-                suiteContent.append(NEW_LINE);
-                suiteContent.append(NEW_LINE);
-                suiteContent.append("@RunWith(org.junit.runners.Suite.class)");
-                suiteContent.append(NEW_LINE);
-                suiteContent.append("@SuiteClasses( {");
-                suiteContent.append(NEW_LINE);
-                boolean firstTime = true;
-                for (String string : includedFiles) {
-                    suiteContent.append("\t");
-                    if (firstTime) {
-                        firstTime = false;
-                    } else {
-                        suiteContent.append(",");
-                    }
-                    String pattern = FILE_SEPARATOR;
-                    if ("\\".equals(FILE_SEPARATOR)) {
-                        // we have to quote the back slash
-                        pattern = "\\" + FILE_SEPARATOR;
-                    }
-                    suiteContent.append(string.replaceAll(pattern, ".").replaceAll("java", "class"));
-                    suiteContent.append(NEW_LINE);
-                }
-                suiteContent.append("} )");
-                suiteContent.append(NEW_LINE);
-                suiteContent.append("public class AllTestsSuite { }");
-                suiteContent.append(NEW_LINE);
-                fileWriter.write(suiteContent.toString());
-            }
+			String[] includedFiles = scanner.getIncludedFiles();
 
-        } catch (IOException ex) {
-            throw new MojoFailureException("Could not write output file.");
-        } finally {
-            try {
-                if (fileWriter != null) {
-                    fileWriter.close();
-                }
-            } catch (IOException ex) {
-                Logger.getLogger(ListMojo.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
+			for (String fileName : includedFiles) {
+				Path file = fileSystem.getPath(baseDir, fileName);
+				JsonObject element = new JsonObject();
+				for (String field : fields)
+					switch (field) {
+					case "name":
+						element.addProperty(field, fileName);
+						break;
+					case "size":
+						element.addProperty(field, (Long) Files.getAttribute(file, "basic:size"));
+						break;
+					case "creationTime":
+						element.addProperty(field, dateFormat.format(
+								new Date(((FileTime) Files.getAttribute(file, "basic:creationTime")).toMillis())));
+						break;
+					case "lastModifiedTime":
+						element.addProperty(field, dateFormat.format(
+								new Date(((FileTime) Files.getAttribute(file, "basic:lastModifiedTime")).toMillis())));
+						break;
+					}
+				array.add(element);
+			}
 
-    }
+			log.info("File list contains " + array.size() + " files");
+
+			if ("json".equals(this.type)) {
+				fileWriter.write(new GsonBuilder().setPrettyPrinting().create().toJson(array));
+			} else if ("junit".equals(this.type)) {
+				StringBuilder suiteContent = new StringBuilder();
+				suiteContent.append("package n4.quat.selenium.acceptancetest.suites;");
+				suiteContent.append(NEW_LINE);
+				suiteContent.append(NEW_LINE);
+				suiteContent.append("import org.junit.runner.RunWith;");
+				suiteContent.append(NEW_LINE);
+				suiteContent.append("import org.junit.runners.Suite.SuiteClasses;");
+				suiteContent.append(NEW_LINE);
+				suiteContent.append(NEW_LINE);
+				suiteContent.append("@RunWith(org.junit.runners.Suite.class)");
+				suiteContent.append(NEW_LINE);
+				suiteContent.append("@SuiteClasses( {");
+				suiteContent.append(NEW_LINE);
+				boolean firstTime = true;
+				for (String string : includedFiles) {
+					suiteContent.append("\t");
+					if (firstTime) {
+						firstTime = false;
+					} else {
+						suiteContent.append(",");
+					}
+					String pattern = FILE_SEPARATOR;
+					if ("\\".equals(FILE_SEPARATOR)) {
+						// we have to quote the back slash
+						pattern = "\\" + FILE_SEPARATOR;
+					}
+					suiteContent.append(string.replaceAll(pattern, ".").replaceAll("java", "class"));
+					suiteContent.append(NEW_LINE);
+				}
+				suiteContent.append("} )");
+				suiteContent.append(NEW_LINE);
+				suiteContent.append("public class AllTestsSuite { }");
+				suiteContent.append(NEW_LINE);
+				fileWriter.write(suiteContent.toString());
+			}
+		} catch (IOException ex) {
+			throw new MojoFailureException("Could not write output file.");
+		}
+	}
 }
